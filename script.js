7719dfe258a038f68e64f3ce69a1eafc6e13388f
Charles042 <ekechukwuma20@gmail.com>
2024-01-26 12:34:41 +0100

    Added a way to use and access them

--- script.js
+++ script.js
@@ -3,12 +3,35 @@
 class Ginger {
      //instance property
 
-    talent1 = 'singing';
-    talent2 = 'sleeping';
-    talent3 = 'writing';
+    talent1 = 'Singing';
+    talent2 = 'Sleeping';
+    talent3 = 'dancing';
     
     //static property
-    
-    static numberOfTalent = 3
-    
+
+    static numberOfTalent = 3;
+// methods
+    bestTalent(){
+        console.log("Singing");
     }
+
+    worseTalent(){
+    console.log("dancing")
+    }
+    };
+//initiating the object
+    const talent = new Ginger();
+
+    //to use or change the values in the object
+    talent.talent1 = "Writing";
+
+    //Accessing the class and static properties
+   
+    console.log(talent);
+    console.log(Ginger.numberOfTalent);
+// Acessing the methods
+   
+    talent.bestTalent();
+    talent.worseTalent();
+
+    
